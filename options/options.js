dfa934f5b06284bdc2ed54ce44aabc780ae177f2
Crowdbotics <engineering@crowdbotics.com>
2023-06-01 14:00:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steep-poetry-dev-87819",
+  url: "https://steep-poetry-dev-87819.botics.co",
+  api: "https://steep-poetry-dev-87819.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
